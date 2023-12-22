29ebb2d24a862a01bf4b2a139dc758e7e083b2b5
Diego Rodriguez <godiedri@gmail.com>
2023-12-22 21:02:28 +0100

    avance web dra carolina, navbar, primera seccion

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      "color-primary": '#729343',
+      "color-primary-dark": "#096A2E",
+      "color-secondary": "#ffaf00",
+      "color-secondary-light": "#f1b805",
+      "color-gray": "#333",
+      "color-white": "#fff",
+      "color-envelope": "#ff0000",
+      "color-primary-light": "#ecedf0",
+    },
+  },
+  plugins: [],
+}
+
